f40ab3e7001561c64ffb4b737b26c946edfed58c
Baran12344 <69864153+Baran12344@users.noreply.github.com>
2022-09-04 13:34:23 +0300

    Update projects.jsx

--- pages/api/projects.jsx
+++ pages/api/projects.jsx
@@ -2,8 +2,8 @@ export default async (req, res) => {
   const data = [
     {
       id: 1,
-      image: "https://media.discordapp.net/attachments/737348411568685066/953718636458180618/images.png",
-      name: "Hydra.M Bot",
+      image: "https://media.discordapp.net/attachments/904763470623428738/1015932314615824424/sake-black-death-smile-wallpaper-preview.jpg",
+      name: "DDoS music bot",
       description:
         "Kendi discord sunucunuzda kullana bileceğiniz türkçe ve ingilizce dil destekli mükemmel müzik botu.",
       link: "https://discord.com/oauth2/authorize?client_id=746109878149447771&permissions=8&scope=bot",
