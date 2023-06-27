266a99477dd123ca7d0521a2878beb8fc5568b79
agony01 <115938347+agony01@users.noreply.github.com>
2023-06-27 17:36:55 +0300

    Update projects.jsx

--- pages/api/projects.jsx
+++ pages/api/projects.jsx
@@ -3,7 +3,7 @@ export default async (req, res) => {
     {
       id: 1,
       image: "https://cdn.discordapp.com/attachments/937770732295975033/1123259147056582727/g-letter-with-abstract-glitch-effect-vector.jpg",
-      name: "DDoS music bot",
+      name: "Gececi team",
       description:
         "Çeşitli eğlencelerle dolu Discord sunucumuza hepinizi davet ediyoruz!",
       link: "https://discord.gg/BnbasZ269c",
